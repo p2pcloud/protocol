74842bc61c93f67172be0646ee5b02fb1243e3f3
Your Name <you@example.com>
2023-01-05 21:18:40 +0000

    testnet deploy

--- scripts/upgrade-broker-testnet.ts
+++ scripts/upgrade-broker-testnet.ts
@@ -1,11 +1,18 @@
 import { ethers, upgrades } from "hardhat";
 
-const PROXY_ADDRESS = "0x951e84e6e252355724bf59F741f5CAEf89CA2Bc2";
+let PROXY_ADDRESS = "0x935df18a119F5CfD13d12c1cAaF74c004869592F";
 
 async function main() {
-    const Contract = await ethers.getContractFactory("BrokerV1");
-    const instance = await upgrades.upgradeProxy(PROXY_ADDRESS, Contract);
-    console.log("Contract upgraded");
+    if (PROXY_ADDRESS === "") {
+        const Broker = await ethers.getContractFactory("BrokerV1");
+        const broker = await upgrades.deployProxy(Broker);
+        await broker.deployed();
+        console.log("BrokerV1 deployed to:", broker.address);
+    } else {
+        const Contract = await ethers.getContractFactory("BrokerV1");
+        await upgrades.upgradeProxy(PROXY_ADDRESS, Contract);
+        console.log("Contract upgraded");
+    }
 }
 
 main().catch((error) => {
