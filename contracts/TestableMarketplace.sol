e527db493fb97685186054afc6695fe86c43263f
solohin <solohin.i@gmail.com>
2023-04-25 03:37:17 +0000

    GetOfferSigner for golang dev

--- contracts/TestableMarketplace.sol
+++ contracts/TestableMarketplace.sol
@@ -3,13 +3,41 @@
 pragma solidity ^0.8.17;
 
 import "./Marketplace.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract TestableMarketplace is Marketplace {
+    using ECDSA for bytes32;
+
     function test__getTime() public view returns (uint256) {
         return block.timestamp;
     }
 
     function test__increaseSpendingPerMinute(address user, uint256 amt) public {
         _totalSpendingPerMinute[user] += amt;
+    }
+
+    function test__GetOfferSigner(
+        UnsignedOffer calldata offer,
+        bytes calldata signature
+    ) public view returns (address) {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(
+                        OFFER_TYPEHASH,
+                        offer.client,
+                        offer.pricePerMinute,
+                        offer.nonce,
+                        offer.specs,
+                        offer.expiresAt
+                    )
+                )
+            )
+        );
+
+        address recoveredSigner = digest.recover(signature);
+        return resolveSigner(recoveredSigner);
     }
 }
