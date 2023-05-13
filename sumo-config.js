58d9974020d62ae57fe2d3f78d433bed4fd8ef61
solohin <solohin.i@gmail.com>
2023-05-13 22:15:32 +0000

    add sumo

---
+++ sumo-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    buildDir: '',
+    contractsDir: '',
+    testDir: '',
+    skipContracts: [],
+    skipTests: [],
+    testingTimeOutInSec: 300,
+    network: "none",
+    testingFramework: "hardhat",
+    minimal: false,
+    tce: false
+}
