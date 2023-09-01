8792c28c096afe044078531bda4eabaafe51ff8e
solohin <solohin.i@gmail.com>
2023-09-01 14:51:04 +0000

    VerifiableKYC test

--- test/VerifiableKYC.test.ts
+++ test/VerifiableKYC.test.ts
@@ -2,7 +2,33 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { deployMarketplaceV3Fixture } from './fixtures'
+import { NZ_HEX, US_HEX, signKYC } from "./lib";
 
 describe("VerifiableKYC", function () {
-    it("should not accept if receiver address or country is rigged")
+    it("should not accept if receiver address or country is rigged", async function () {
+        const { marketplace, user, providersSigner, anotherUser, kycSigner } = await loadFixture(deployMarketplaceV3Fixture);
+
+        //wrong signer
+        await expect(marketplace.connect(anotherUser).submitKYC(
+            anotherUser.address, US_HEX,
+            await signKYC(anotherUser.address, US_HEX, user)
+        )).to.be.revertedWithCustomError(marketplace, "InvalidKYCSigner")
+
+        //rig country
+        await expect(marketplace.connect(anotherUser).submitKYC(
+            anotherUser.address, US_HEX,
+            await signKYC(anotherUser.address, NZ_HEX, kycSigner)
+        )).to.be.revertedWithCustomError(marketplace, "InvalidKYCSigner")
+
+        //rig address
+        await expect(marketplace.connect(anotherUser).submitKYC(
+            anotherUser.address, US_HEX,
+            await signKYC(user.address, US_HEX, kycSigner)
+        )).to.be.revertedWithCustomError(marketplace, "InvalidKYCSigner")
+
+        await marketplace.connect(anotherUser).submitKYC(
+            anotherUser.address, US_HEX,
+            await signKYC(anotherUser.address, US_HEX, kycSigner)
+        )
+    })
 }) 
